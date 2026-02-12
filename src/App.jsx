7ae92a9df8027174f9c3aa2fd28000e2ca2d45ee
Kalididdi Jayarohit Varma <116787916+rohit-kodnest@users.noreply.github.com>
2026-02-12 22:28:03 +0530

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
